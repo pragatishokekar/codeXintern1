749843df931801fcbf9550d78082954fc5997a9c
Pragati Shokekar <pragatishokekar@gmail.com>
2025-01-23 14:52:01 +0530

    Add files via upload

---
+++ slab1.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".btn")
+const container  = document.querySelector(".container")
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    if (btn.innerText === "Light"){
+        container.style.backgroundColor = "black"
+        btn.innerText = "Dark"
+    } else {
+        container.style.backgroundColor = "white"
+        btn.innerText = "Light"
+    }
+})
